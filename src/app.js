53fd678039e1fe472d86d1edec47813e554a492e
DESKTOP-GGKHKBC\PEDRO <diegoedgardosalazarvergara@gmail.com>
2020-12-04 23:20:45 -0500

    RomPedFinal

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ start.onclick = () =>{
     document.querySelector(".box").style.opacity = 1;
     document.getElementById("buttonStart").style.opacity=0;
     document.getElementById("audio").play();
+    document.getElementById("audio").volume = 0.3;
     setTimeout(() =>{
         document.getElementById("container").style.opacity=1;
         document.getElementById("buttonStart").style.display = 'none';
